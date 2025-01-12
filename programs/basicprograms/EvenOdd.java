4e6665db183dbc17efe82bdf593c2fb6c86c2eed
Varad Mule <varadmule17@gmail.com>
2025-01-12 19:58:41 +0530

    1

---
+++ programs/basicprograms/EvenOdd.java
@@ -0,0 +1,20 @@
+package programs.basicprograms;
+
+public class EvenOdd {
+
+	public static void main(String[] args) 
+	{
+		int n = 3;
+
+		if (n % 2 == 0) 
+		{
+			System.out.println(n + " is even number");
+		}
+
+		else 
+		{
+			System.out.println(n + " is odd number");
+		}
+	}
+
+}
