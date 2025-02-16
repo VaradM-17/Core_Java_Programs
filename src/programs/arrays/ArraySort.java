f00baad9dfa24ec4f78f215ec1f54491ae40bb52
Varad Mule <varadmule17@gmail.com>
2025-02-17 03:41:17 +0530

    Array Max Digit Addition added

--- src/programs/arrays/ArraySort.java
+++ src/programs/arrays/ArraySort.java
@@ -4,7 +4,7 @@ public class ArraySort {
 	public static void main(String[] args) {
 
 		int a[] = { 7, 2, 10, 5 };
-		
+
 		System.out.print("Sorted Array : ");
 		for (int i = 0; i < a.length; i++) {
 			for (int j = 0; j < a.length; j++) {
