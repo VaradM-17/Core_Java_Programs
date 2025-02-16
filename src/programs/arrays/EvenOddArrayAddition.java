5395939ab3729c040104c6b2980b59b96f1fc3b4
Varad Mule <varadmule17@gmail.com>
2025-02-17 02:20:56 +0530

    EvenOddArrayAddition added

--- src/programs/arrays/EvenOddArrayAddition.java
+++ src/programs/arrays/EvenOddArrayAddition.java
@@ -1,9 +1,20 @@
 package programs.arrays;
 
 public class EvenOddArrayAddition {
-
 	public static void main(String[] args) {
+		int a[] = { 7, 2, 10, 5 };
+		int evenSum = 0;
+		int oddSum = 0;
 
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] % 2 == 0) {
+				evenSum = evenSum + a[i];
+			} else {
+				oddSum = oddSum + a[i];
+			}
+		}
+		System.out.println("Addition of even number is " + evenSum);
+		System.out.println("Addition of odd number is " + oddSum);
 	}
 
 }
