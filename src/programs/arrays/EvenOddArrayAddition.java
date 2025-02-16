63a9e338f1a22a51f658f222993f358f8a18f235
Varad Mule <varadmule17@gmail.com>
2025-02-17 02:13:19 +0530

    Array Addition Added

---
+++ src/programs/arrays/EvenOddArrayAddition.java
@@ -0,0 +1,9 @@
+package programs.arrays;
+
+public class EvenOddArrayAddition {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/programs/arrays/MaxNumber.java
+++ src/programs/arrays/MaxNumber.java
@@ -2,6 +2,14 @@ package programs.arrays;
 
 public class MaxNumber {
 	public static void main(String[] args) {
+		int a[] = { 12, 18, 11,21, 17 };
+		int max = a[0];
 
+		for (int i = 1; i < a.length; i++) {
+			if (max < a[i]) {
+				max = a[i];
+			}
+		}
+		System.out.println("Maximum Value is : " + max);
 	}
 }
