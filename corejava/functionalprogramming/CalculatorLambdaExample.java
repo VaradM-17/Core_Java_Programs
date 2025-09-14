843515edb932f64e8c1669cc6a00afc711cf4aaa
Varad Mule <varadmule17@gmail.com>
2025-09-14 12:08:08 +0530

    lambda expression as parameter example

--- corejava/functionalprogramming/CalculatorLambdaExample.java
+++ corejava/functionalprogramming/CalculatorLambdaExample.java
@@ -13,11 +13,21 @@ interface Calculator {
 //}
 
 public class CalculatorLambdaExample {
+
+	public static int calculate(int a, int b, Calculator calc) {
+		return calc.calculate(a, b);
+	}
+
 	public static void main(String[] args) {
+		// Without lambda
 //		Calculator add = new Addition();
 //		System.out.println(add.calculate(14, 15));
 
-		Calculator addition = (a, b) -> a + b;
-		System.out.println(addition.calculate(14, 15));
+		// With lambda
+//		Calculator addition = (a, b) -> a + b;
+//		System.out.println(addition.calculate(14, 15));
+
+		// Pass lambda as parameter
+		System.out.println(calculate(10, 20, (a, b) -> a + b));
 	}
 }
--- corejava/functionalprogramming/LambaExpressionExample.java
+++ corejava/functionalprogramming/LambaExpressionExample.java
@@ -22,16 +22,30 @@ interface Shape {
 //}
 
 public class LambaExpressionExample {
+	// pass lambda as parameter
+	public static void drawing(Shape shape) {
+		shape.draw();
+	}
+
 	public static void main(String[] args) {
 //		Shape rectangle = new Rectangle();
 //		rectangle.draw();
 //
 //		Shape square = new Square();
 //		square.draw();
-		Shape rectangle = () -> System.out.println("Rectangle is drawing...");
-		rectangle.draw();
+//
+//		Shape rectangle = () -> System.out.println("Rectangle is drawing...");
+//		rectangle.draw();
+//
+//		Shape square = () -> System.out.println("Square is drawing...");
+//		square.draw();
 
-		Shape square = () -> System.out.println("Square is drawing...");
-		square.draw();
+//      pass lambda as parameter
+//		Shape circle = () -> System.out.println("Drawing circle ...");
+//		drawing(circle);
+
+		drawing(() -> System.out.println("Drawing rectangle..."));
+		drawing(() -> System.out.println("Drawing circle..."));
+		drawing(() -> System.out.println("Drawing square..."));
 	}
 }
