709f50831e0774708dd001551247f258b085378e
Varad Mule <varadmule17@gmail.com>
2025-10-28 22:40:29 +0530

    more pattern programs added

--- programs/patterns/InvertedTrianglePattern.java
+++ programs/patterns/InvertedTrianglePattern.java
@@ -2,20 +2,18 @@ package com.programs.patterns;
 
 import java.util.Scanner;
 
-public class HalfRightPyramid {
+public class InvertedTrianglePattern {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a number: ");
-
+		System.out.print("Enter a number: ");
 		int num = sc.nextInt();
 
-		for (int i = 1; i <= num; i++) {
+		for (int i = num; i > 0; i--) {
 			for (int j = 1; j <= i; j++) {
-				System.out.print(i + " ");
+				System.out.print(j + " ");
 			}
 			System.out.println();
 		}
-
 		sc.close();
 	}
 }
--- programs/patterns/MirrorRightTrianglePattern.java
+++ programs/patterns/MirrorRightTrianglePattern.java
@@ -2,20 +2,22 @@ package com.programs.patterns;
 
 import java.util.Scanner;
 
-public class HalfRightPyramid {
+public class MirrorRightTrianglePattern {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a number: ");
-
+		System.out.print("Enter a number: ");
 		int num = sc.nextInt();
 
 		for (int i = 1; i <= num; i++) {
-			for (int j = 1; j <= i; j++) {
-				System.out.print(i + " ");
+			for (int j = 1; j <= num; j++) {
+				if (j <= num - i) {
+					System.out.print(" ");
+				} else {
+					System.out.print(j);
+				}
 			}
 			System.out.println();
 		}
-
 		sc.close();
 	}
 }
