2ad2a40f11974537d01ac89c4d2f8e3442459163
Varad Mule <varadmule17@gmail.com>
2026-03-02 19:16:19 +0530

    added linear search algorithm example

---
+++ programs/searching/LinearSearchExample.java
@@ -0,0 +1,22 @@
+package com.programs.searching;
+
+public class LinearSearchExample {
+	public static void main(String[] args) {
+
+		int arr[] = { 10, 20, 30, 40 };
+		int findElement = 30;
+		boolean found = false;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == findElement) {
+				System.out.println("Element found at index " + i);
+				found = true;
+				break;
+			}
+		}
+		if (!found) {
+			System.out.println("Element not found.");
+		}
+
+	}
+}
