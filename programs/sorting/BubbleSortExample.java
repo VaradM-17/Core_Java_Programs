d2d937a7b4e26e8a18810c110597bfde61b63011
Varad Mule <varadmule17@gmail.com>
2026-03-02 22:05:49 +0530

    updated bubble sort algorithm program

--- programs/sorting/BubbleSortExample.java
+++ programs/sorting/BubbleSortExample.java
@@ -7,23 +7,24 @@ public class BubbleSortExample {
 		int num[] = { 10, 1, 3, 4, 6, 8 };
 		int length = num.length;
 
-		int temp = 0;
-		for (int i = 1; i < length; i++) {// passes
+		for (int i = 0; i < length - 1; i++) { // passes
 
 			boolean swapped = false;
-			for (int j = 0; j < length - i; j++) {// traverse
 
-				if (num[j] > num[j + 1]) {// compare
+			for (int j = 0; j < length - 1 - i; j++) { // traverse
+
+				if (num[j] > num[j + 1]) { // compare
 
 					// swap
-					temp = num[j];
+					int temp = num[j];
 					num[j] = num[j + 1];
 					num[j + 1] = temp;
 
 					swapped = true;
 				}
 			}
-			if (swapped == false) {
+
+			if (!swapped) {
 				break;
 			}
 		}
@@ -31,6 +32,5 @@ public class BubbleSortExample {
 		for (int i = 0; i < num.length; i++) {
 			System.out.print(num[i] + " ");
 		}
-
 	}
 }
